1b3afcb0e18fbd473d20e37d59fc89d9a3b0b292
Aedan <aedan.giffin@gmail.com>
2019-02-07 22:44:29 +0000

    Complete without validation feauter

--- index.js
+++ index.js
@@ -1,29 +1,66 @@
-let balance = 500.00;
-
-class Withdrawal {
-
-  constructor(amount) {
+class Transaction {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    this.time = new Date();
+    this.account.addTransaction(this);
+    console.log(this.value);
+  }
+}
+
+class Withdrawal extends Transaction {
+  get value() {
+    return this.amount * -1;
+  }
+
+
+}
+class Deposit extends Transaction {
+
+  get value() {
+    return this.amount;
   }
 
 }
 
+class Account {
+
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
+  }
+
+  get balance() {
+    let balance = 0;
+    this.transactions.forEach( (transaction) => {
+      balance += transaction.value;
+    })
+    
+    return balance;
+    
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+}
 
 
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
+const myAccount = new Account("JuiceFiend");
 
-t1 = new Withdrawal(50.25);
+t1 = new Deposit(500, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+t3 = new Withdrawal(50.25, myAccount);
+t3.commit();
+
+console.log('Balance:', myAccount.balance);
